5207c96b7029f9f9bfb9e2a8fd976e9dfe6d1ed0
Marcos <marcos-aquino@live.com>
2018-11-22 18:05:16 -0200

    Adicionando menu de ações no cadastro de clientes!

--- app/src/main/java/com/ufjf/marcos/clientes/Cadastro.java
+++ app/src/main/java/com/ufjf/marcos/clientes/Cadastro.java
@@ -5,19 +5,69 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Cadastro extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtEndereco;
+    private EditText edtEmail;
+    private EditText edtTelefone;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        edtNome = (EditText) findViewById(R.id.edtNome);
+
+        edtEndereco = (EditText) findViewById(R.id.edtEndereco);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+
+        edtTelefone = (EditText) findViewById(R.id.edtTelefone);
 
     }
 
+
+    // insere o Menu na página de cadastro
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cadastro, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.actCadastrar:
+                Toast.makeText(this, "Botão cadastrar", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.actCancelar:
+                Toast.makeText(this, "Botão cancelar", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
